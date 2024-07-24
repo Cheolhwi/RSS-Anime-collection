a62514980cbef807580986ba8999f694045ef960
Cheolhwi <73050978+Cheolhwi@users.noreply.github.com>
2024-07-24 20:49:55 +0800

    Update run_after_done.sh

--- run_after_done.sh
+++ run_after_done.sh
@@ -8,9 +8,6 @@ directory="$2"
 echo "File name: $file_name" > /tmp/qbittorrent_log.txt
 echo "Directory: $directory" >> /tmp/qbittorrent_log.txt
 
-# 调用外部Python脚本 unwanted.py
-python3 /Volumes/media/anime/unwanted.py
-
 # 运行Python脚本，并获取重命名后的信息
 output=$(python3 /Volumes/media/anime/rename_anime.py --name="$file_name" --root="$directory")
 anime_name=$(echo $output | cut -d',' -f1)
@@ -18,7 +15,14 @@ new_name=$(echo $output | cut -d',' -f2)
 new_path=$(echo $output | cut -d',' -f3)
 
 # 记录Python脚本输出
-echo "Python script output: $output" >> /tmp/qbittorrent_log.txt
+echo "Python script output (rename): $output" >> /tmp/qbittorrent_log.txt
+
+# 切换到目标目录，确保路径正确
+cd /Volumes/media/anime || { echo "Failed to change directory"; exit 1; }
+
+# 运行删除脚本并记录输出
+delete_output=$(python3 unwanted.py)
+echo "Python script output (delete): $delete_output" >> /tmp/qbittorrent_log.txt
 
 # 如果重命名成功并且新路径不为空
 if [ -n "$anime_name" ] && [ -n "$new_name" ] && [ -n "$new_path" ]; then
